e86bb194d2c74b32a2b8b6748b7a1d1fdd73e2a7
Jacob_Mac <jacob7395@gmail.com>
2016-11-21 17:37:32 +0000

    Factory Now Makes classes with more logical IDs

--- Code/JacobLib/Class_Factory.py
+++ Code/JacobLib/Class_Factory.py
@@ -9,12 +9,14 @@ class Class_Factory:
 
     def New(self, *args):
         """Generate new class and incroment ID."""
-        name = self.name + str(self.classID)
+        # Make a new class using the passed base class
+        Class = self.name + str(self.classID)
         self.classID += 1
-        name = type(name, (self.baseClass,), {})
-
+        Class = type(Class, (self.baseClass,), {})
+        # generate this new class with spesific ID info
         new_Class = self.baseClass(*args)
-        new_Class.__class__ = name
-        new_Class.__name__ = self.name + str(self.classID)
+        new_Class.__class__ = Class
+        new_Class.__name__ = self.name + "ID" + str(self.classID)
+        new_Class.class_Name = self.name
         new_Class.ID = self.classID
         return new_Class
