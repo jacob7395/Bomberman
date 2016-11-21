dcdd82d0c2faec8e57aee53defe11bd405bb945b
Jacob_Mac <jacob7395@gmail.com>
2016-11-21 17:39:57 +0000

    Implemented BMan and simple controls with basic collations
    Very simple controls for BMan and BMen now spawn on all spawn blocks.
    Basic contour using wads
    collisions sorta work but not really

--- Code/main.py
+++ Code/main.py
@@ -29,11 +29,19 @@ from Class_Factory import Class_Factory
 sys.path.insert(0, path_Sprite)
 # import bomb sprite
 from Bomb import Sprite_Bomb
+# import bomberman sprite
+from Bomberman import Sprite_Bomberman
 
 sys.path.insert(0, path_Map_Gen)
 # import test sprite
 from Map_Maker import Map_Load
 
+
+def sign(x):
+    if (x <= 0):
+        return 1
+    else:
+        return -1
 
 # -----------------------------------------------
 # Reset error file
@@ -61,14 +69,25 @@ pygame.display.set_caption("Bomberman")
 # added to this list. The list is managed by a class called 'Group.'
 # make backgroud using map reader Function
 # background runs in a threadList
-sprite_Lists = Map_Load(screen_Size)
+sprite_Lists = Map_Load(screen_Size, 21)
 background_List = sprite_Lists[0]
+bDrwan = False
 bush_List = sprite_Lists[1]
-sprite_Scale = sprite_Lists[2]
+wall_List = sprite_Lists[2]
+sprite_Scale = sprite_Lists[3]
 # bomb group and factry init
 bomb_List = pygame.sprite.Group()
-bomb_Factory = Class_Factory("BombID", Sprite_Bomb)
-bomb_Queue = Queue()
+bomb_Factory = Class_Factory("Bomb", Sprite_Bomb)
+# make bomber man
+man_List = pygame.sprite.Group()
+man_Factory = Class_Factory("Man", Sprite_Bomberman)
+man_Count = 0
+for x in background_List:
+    if(x.class_Name == "Spawn"):
+        man = man_Factory.New(x.Position(), False, sprite_Scale, man_Count)
+        man.Alphe_Con()
+        man_List.add(man)
+        man_Count += 1
 # adds alpha color for bushes, color = default = BLACK = (0,0,0)
 for sprite in bush_List:
     sprite.Alphe_Con()
@@ -77,23 +96,6 @@ for sprite in bush_List:
 done = False
 # used to manage how fast the screen updates
 clock = pygame.time.Clock()
-# -----------------------------------------------
-# COLLISTION TEST CODE
-# collitions = groupcollide(pepe_List, pepe_List, False, False)
-# collitions_Keys = collitions.keys()
-# collitions_Values = collitions.values()
-#
-# for i in range(len(collitions.keys())):
-#     l = list(str(collitions_Keys[i]))
-#     x = l.index(' ')
-#     collitions_Keys[i] = str(collitions_Keys[i])[1:x]
-#
-# for values in range(len(collitions_Values)):
-#     for elements in range(len(collitions_Values[values])):
-#         l = list(str(collitions_Values[values][elements]))
-#         x = l.index(' ')
-#         collitions_Values[values][elements] = str(collitions_Values[values][elements])[1:x]
-# -------------------------------------------------------------
 # -------- Main Program Loop -----------
 while not done:
     # --- Main event loop
@@ -110,33 +112,109 @@ while not done:
                     scree_Fullscreen = True
             elif event.key == pygame.K_F2:
                 done = True
+            elif event.key == pygame.K_w:
+                for man in man_List:
+                    if man.ID == 1:
+                        man.current_Direction = "man_Up"
+                        man.begin_Run()
+            elif event.key == pygame.K_s:
+                for man in man_List:
+                    if man.ID == 1:
+                        man.current_Direction = "man_Down"
+                        man.begin_Run()
+            elif event.key == pygame.K_d:
+                for man in man_List:
+                    if man.ID == 1:
+                        man.current_Direction = "man_Right"
+                        man.begin_Run()
+            elif event.key == pygame.K_a:
+                for man in man_List:
+                    if man.ID == 1:
+                        man.current_Direction = "man_Left"
+                        man.begin_Run()
+            elif event.key == pygame.K_SPACE:
+                for man in man_List:
+                    if man.ID == 1:
+                        bomb = bomb_Factory.New(man.Position(), False, sprite_Scale, 1)
+                        bomb.Alphe_Con()
+                        bomb_List.add(bomb)
+
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_w:
+                for man in man_List:
+                    if man.ID == 1:
+                        if(man.current_Direction == "man_Up"):
+                            man.stop_Run()
+            elif event.key == pygame.K_s:
+                for man in man_List:
+                    if man.ID == 1:
+                        if(man.current_Direction == "man_Down"):
+                            man.stop_Run()
+            elif event.key == pygame.K_a:
+                for man in man_List:
+                    if man.ID == 1:
+                        if(man.current_Direction == "man_Left"):
+                            man.stop_Run()
+            elif event.key == pygame.K_d:
+                for man in man_List:
+                    if man.ID == 1:
+                        if(man.current_Direction == "man_Right"):
+                            man.stop_Run()
+        # Mouse click event currently spawning bomb
         elif event.type == pygame.MOUSEBUTTONDOWN:
             pos = mouse.get_pos()
-            bomb = bomb_Factory.New(pos, False, sprite_Scale, 2)
+            bomb = bomb_Factory.New(pos, False, sprite_Scale, 1)
             bomb.Alphe_Con()
             bomb_List.add(bomb)
 
     # --- Game logic should go here
+    # Calculate collitions with Bman
+    man_Collitions = pygame.sprite.groupcollide(man_List, wall_List, False, False)
+    man_Collitions.update(pygame.sprite.groupcollide(man_List, bush_List, False, False))
+    if(man_Collitions):
+        for man in man_Collitions.keys():
+            man.stop_Run()
+            collition_Pos = man_Collitions[man][0].Position()
+            man.Position(man.position_Old[0], man.position_Old[1])
     # Update curret bombs
     for bomb in bomb_List:
         if(bomb.update() == True):
             bomb_List.remove(bomb)
-    # --- Screen-clearing code goes here
+    for man in man_List:
+        man.update()
+        if(man.ID == 1):
+            pass
+            # print("Pos:" + str(man.Position()))
 
-    # Here, we clear the screen to white. Don't put other drawing commands
-    # above this, or they will be erased with this command.
+            # if(man.current_Direction == "man_Down" or man.current_Direction == "man_Up"):
+            #     S = sign(collition_Pos[1] - man.Position()[1])
+            #     man.position_y = collition_Pos[1] + sprite_Scale * S
+            # elif(man.current_Direction == "man_Left" or man.current_Direction == "man_Right"):
+            #     S = sign(collition_Pos[0] - man.Position()[0])
+            #     man.position_x = collition_Pos[0] + sprite_Scale * S
 
-    # If you want a background image, replace this clear with blit'ing the
-    # background image.
+            # --- Screen-clearing code goes here
+
+            # Here, we clear the screen to white. Don't put other drawing commands
+            # above this, or they will be erased with this command.
+
+            # If you want a background image, replace this clear with blit'ing the
+            # background image.
     screen.fill(BLACK)
     # --- Drawing code should go here
-    background_List.draw(screen)
-    bush_List.draw(screen)
-    bomb_List.draw(screen)
+    if(bDrwan == False):
+        background_List.draw(screen)
+        wall_List.draw(screen)
+        bush_List.draw(screen)
+        bomb_List.draw(screen)
+        bDrawn = True
+    man_List.draw(screen)
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
     # --- Limit to 60 frames per second
     clock.tick(60)
+    if(clock.get_fps() > 20):
+        print ("fps:" + str(clock.get_fps()))
 
 # Close the window and quit.
 pygame.quit()
